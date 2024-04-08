bbe14ee9a965784f19e710e98134e3c67490d309
Marickyare <83879224+Marickyare@users.noreply.github.com>
2024-04-08 22:23:17 +0700

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <math.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float x, y;
+
+	printf("Введите координату x: "); scanf_s("%f", &x);
+	printf("Введите координату y: "); scanf_s("%f", &y);
+
+	if ((sqrt(x) + sqrt(y)) <= 2 and (sqrt(x) + sqrt(y)) >= 1)
+	{
+		if (x > 0)
+		{
+			printf("Координаты подходят.");
+		}
+		else
+		{
+			printf("Координаты не подходят, введите пложительный x.");
+		}
+	}
+	else
+	{
+		printf("Координаты не подходят, введите дргие.");
+	}
+	return 0;
+}
