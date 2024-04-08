9dc71d53fabeb3ad5fc8f6b1d157da114b5c4683
Marickyare <83879224+Marickyare@users.noreply.github.com>
2024-04-08 22:06:19 +0700

    Add files via upload

---
+++ lab2.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Hello World!\n";
+    float a, b, F;
+
+
+    cout << "Введите положительное значение a: "; cin >> a; //т.к. неветвл. алг., вводим усл. для польз. вручную
+    cout << "Введите неотрицтельное значение b: "; cin >> b; //т.к. неветвл. алг., вводим усл. для польз. вручную
+    float Tnumerator = 8.15 * pow(b, 3) * log(a);
+    float Fdenominator = 24.38 * cos(b) * (exp(a) - pow(a, a));
+    F = Tnumerator / Fdenominator;
+    cout << "Значение выражения: " << F;
+    return 0;
+}
