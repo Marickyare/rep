4d23e7724e8b0aa647a88f897eca459f8e988135
Marickyare <83879224+Marickyare@users.noreply.github.com>
2024-04-15 18:52:23 +0700

    Add files via upload

---
+++ lab2.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <math.h>
+#include <locale.h>
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+	float E, S, y;
+	int n = 1;
+	S = 0;
+	printf("Введите значение E: \n"); 
+	cin >> E;
+	
+	if (E >= (1 / 3)) {
+		printf("Введите меньшее значение.");
+	}
+	else {
+		do {
+			y = (n + 5) / (pow(2, n) + 2);
+			S += y;
+			n += 1;
+		} while ((y > E) or ((y - (((n - 2) + 5) / (pow(2, (n - 2)) + 2))) >= E));
+	}
+	printf("%d", n-1);
+
+	return 0;
+
+}
